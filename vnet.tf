b7a485d3e381af2c917c767f99e9108cfab8b48e
Jonathan Davies <jonathan.davies@mediacom.com>
2021-06-29 14:48:44 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "iacdev.wpp.cloud/WPP-Lab/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.name
+  resource_group_name = "${var.prefix}-workshop"
 }
